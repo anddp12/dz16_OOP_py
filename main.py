9150d85fed7f668ec3681d0938e17c0578060094
anddp12 <anddp12@gmail.com>
2022-12-14 23:36:02 +0200

    create class and method

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 # Создайте класс для конвертирования температуры из Цельсия в Фаренгейт и наоборот. 
 # У класса должно быть два статических метода: для перевода из Цельсия в Фаренгейт и для перевода из Фаренгейта в Цельсий. 
+
+
+class Temperature:
+    @staticmethod
+    def Celcii(Farengeit):
+        celcii = celcii
+        return (celcii * 9/5) + 32
+
+    @staticmethod
+    def Farengeit(Celcii):
+        farengeit = farengeit
+        return ((farengeit - 32) * 5) / 9
+
